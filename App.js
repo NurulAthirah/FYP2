4e859b4b15cce02f97e6fc1ecb279c7777f76932
a_sweetscent <athirah.borhan2980@gmail.com>
2021-11-24 08:48:04 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./Screens/HomeScreen";
-import DetailsScreen from "./Screens/DetailsScreen";
-import FormScreen from "./Screens/FormScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import DetailsScreen from "./src/screens/DetailsScreen";
+import { db } from './src/config/db';
+import firebase from 'firebase';
+
+//import FormScreen from "./src/screens/FormScreen";
 
 const Stack = createNativeStackNavigator();
+
+
+
 
 function NavStack()
 {
@@ -21,16 +27,44 @@ function NavStack()
         component={DetailsScreen} 
         options={{ title: 'Details' }}
       />
-      <Stack.Screen 
-       name="Form" 
-       component={FormScreen} 
-       options={{ title: 'Form' }}
-      />
+    
     </Stack.Navigator>
   );
 }
 
 export default class App extends Component {
+
+  componentWillMount() {
+
+  firebase.database().ref('users/004').set(
+    {
+      name: 'aimi',
+      age: 21
+    }
+  ).then(() => {
+    console.log('INSERTED');
+  }).catch((error) => {
+    console.log(error);
+  });
+
+  firebase.database().ref('users/001').set(
+    {
+      name: 'tirah',
+      age: 21
+    }
+  ).then(() => {
+    console.log('INSERTED');
+  }).catch((error) => {
+    console.log(error);
+  });
+
+  firebase.database().ref('users/').on('value', function (snapshot) {
+    console.log(snapshot.val()) //display data taht's changed from firebase on console only 
+});
+}
+
+
+  
   render() {
     return (
       <NavigationContainer>
