963fc836a3eefc1325712b268e5f9313e854d753
a_sweetscent <athirah.borhan2980@gmail.com>
2021-12-01 06:07:10 +0800

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,18 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
+import React, { Component, useState } from "react";
+import { View, Text, StyleSheet, Button, TouchableOpacity,SafeAreaView, ScrollView, Modal } from "react-native";
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faHome,faShoppingBag,faUser, faFileInvoiceDollar } from '@fortawesome/free-solid-svg-icons';
+import { faHome,faShoppingBag,faUser, faFileInvoiceDollar, faPlusCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import BottomSheet from 'reanimated-bottom-sheet';
+import Animated from 'react-native-reanimated'
+import { useNavigation } from '@react-navigation/native';
 
+export default function HomeScreen ({}) {
 
-export default class HomeScreen extends Component {
-  render() {
+const [ModalOpen, setModalOpen] = useState(true)
+
+const navigation = useNavigation();
     return (
- 
-<View style={styles.container}>
+<SafeAreaView style={styles.container}>
 <View style={styles.rect}>
   <View style={styles.rect2}></View>
   <View style={styles.rect3}>
@@ -16,41 +20,80 @@ export default class HomeScreen extends Component {
 
 
     <TouchableOpacity
-        onPress={() => this.props.navigation.navigate('Home')}
+        onPress={() => navigation.navigate('Home')}
         style={styles.circle}>
     <FontAwesomeIcon icon={faHome} size={40} style={styles.icon}></FontAwesomeIcon>
+    <Text style={styles.icontext}>Home</Text>
     </TouchableOpacity>
 
-
     <TouchableOpacity
-        onPress={() => this.props.navigation.navigate('Home')}
+        onPress={() =>  navigation.navigate('Details')}
         style={styles.circle}>
     <FontAwesomeIcon icon={faShoppingBag} size={32} style={styles.icon1}></FontAwesomeIcon>
+    <Text style={styles.icontext}>Products</Text>
     </TouchableOpacity>
     
     <TouchableOpacity
-        onPress={() => this.props.navigation.navigate('Home')}
+        onPress={() =>  navigation.navigate('Home')}
         style={styles.circle}>
     <FontAwesomeIcon icon={faFileInvoiceDollar} size={32} style={styles.icon1}></FontAwesomeIcon>
+    <Text style={styles.icontext}>Records</Text>
     </TouchableOpacity>
     
     <TouchableOpacity
-        onPress={() => this.props.navigation.navigate('Home')}
+        onPress={() =>  navigation.navigate('Home')}
         style={styles.circle}>
     <FontAwesomeIcon icon={faUser} size={32} style={styles.icon1}></FontAwesomeIcon>
+    <Text style={styles.icontext}>Profile</Text>
     </TouchableOpacity>
     </View>
   </View>
 </View>
-<View style={styles.button}>
-        <Button onPress={() => this.props.navigation.navigate('Details')} title="Details" />
-        <Button onPress={() => this.props.navigation.navigate('Form')} title="Form" />
-        </View>
-</View>
+
+<Modal 
+visible={ModalOpen}
+animationType={'slide'}
+transparent visible={ModalOpen}>
+  <TouchableOpacity style={styles.modalBg} onPress={() => setModalOpen(false)}>
+    <View style={styles.modalContainer}>
+   
+      <TouchableOpacity 
+      onPress={() =>  navigation.navigate('Details')}>
+      <Text style={styles.text2}>Add Products</Text>
+      </TouchableOpacity>
+      <TouchableOpacity 
+      onPress={() =>  navigation.navigate('Home')}>
+      <Text style={styles.text2}>Add Transactions</Text>
+      </TouchableOpacity>
+      
+    </View>
+    </TouchableOpacity>
+</Modal>
+
+        <TouchableOpacity
+        onPress={() => setModalOpen(true)}
+        style={styles.circle2}>
+    <FontAwesomeIcon icon={faPlusCircle} size={60} color={'#FF367E'} ></FontAwesomeIcon>
+    </TouchableOpacity>
+
+
+    <ScrollView style={styles.scrollView}>
+        <Text style={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+      </ScrollView>
+   
+</SafeAreaView>
 
 
     );
-  }
+  
 }
 const styles = StyleSheet.create({
   container: {
@@ -65,9 +108,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#311D52"
   },
   rect3: {
-    height: 100,
+    height: 130,
     backgroundColor: "white",
-    flexDirection: "row"
+    flexDirection: "row",
+    paddingBottom: 40
   },
   icon: {
     color: "white",
@@ -91,4 +135,49 @@ const styles = StyleSheet.create({
     zIndex: -1,
     margin: 25
   },
+  circle2: {
+    width: 60,  
+    height: 60,   
+    borderRadius: 30,                                                
+    position: 'absolute',    
+    backgroundColor: "white",                                     
+    bottom: 10,                                                    
+    right: 5, 
+    zIndex:99
+
+  },
+  icontext: {
+    textAlign: 'center', // <-- the magic
+    marginTop: 5
+  },
+  text: {
+  fontSize: 42
+  },
+  text2: {
+    color: "white",
+    fontSize: 15,
+    margin: 5
+    },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginTop: 350
+  },
+  modalBg: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    
+  },
+  
+  modalContainer: {
+    width: '60%',
+    backgroundColor: '#FF367E',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    borderRadius: 10,
+    marginBottom: 20,
+    marginRight: 20
+
+  },
+
 });
