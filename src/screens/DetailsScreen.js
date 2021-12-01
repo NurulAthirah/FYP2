2ff389c6e175951c5fa23e9a3ce0262d423f5194
a_sweetscent <athirah.borhan2980@gmail.com>
2021-12-01 22:28:22 +0800

    Update DetailsScreen.js

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,21 +1,48 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
+import PlusIcon from "../components/PlusIcon";
+import { useNavigation } from '@react-navigation/core'
+import {auth}  from '../config/db'
 
-export default class DetailsScreen extends Component {
-  render() {
+const DetailsScreen = () => {
+
+  const navigation = useNavigation()
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.navigate("SignUp")
+        console.log('Logged out');
+      })
+      .catch(error => alert(error.message))
+  }
+
+  
     return (
       <View style={styles.container}>
         <View>
         <Text>Details Screen</Text>
         </View>
         <View style={styles.button}>
-        <Button onPress={() => this.props.navigation.navigate('Home')} title="Home" />
-        <Button onPress={() => this.props.navigation.navigate('Form')} title="Form" />
+        <Button onPress={() => navigation.navigate('Home')} title="Home" />
+        <Button onPress={() => navigation.navigate('Form')} title="Form" />
         </View>
+        <Text>Email: {auth.currentUser?.email}</Text>
+      <TouchableOpacity
+        onPress={handleSignOut}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Sign out</Text>
+      </TouchableOpacity>
+        <PlusIcon/>
       </View>
+     
     );
-  }
+  
 }
+
+export default DetailsScreen
 
 const styles = StyleSheet.create({
   container: {
