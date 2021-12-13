38ccffb089f77f2a79872df5a5659a904baa6edd
alifah99 <55783173+alifah99@users.noreply.github.com>
2021-12-13 22:11:38 +0800

    Update SignUpScreen.js

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -2,8 +2,11 @@ import { useNavigation } from '@react-navigation/core'
 import React, { useEffect, useState, useContext } from 'react'
 import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native'
 import RNRestart from 'react-native-restart';
+import LinearGradient from 'react-native-linear-gradient';
+import {auth}  from '../config/db';
+import { color } from 'react-native-reanimated';
 
-import {auth}  from '../config/db'
+
 
 
 const SignUpScreen = () => {
@@ -59,17 +62,29 @@ const SignUpScreen = () => {
   return (  
     <View
     style={styles.container}
-    behavior="padding"
-  >
+    behavior="padding">
+      <LinearGradient
+        // Background Linear Gradient
+        colors={['#311D52', '#311D52','#FFFFFF','transparent']}
+        style={styles.background}
+      />
+      
+      <Text style={{color: '#FFFFFF',fontSize: 40 }}>Log Help</Text>
+      <Text style={{color: '#FFFFFF',fontSize: 20}}>Inventory System Management</Text>
+
     <View style={styles.inputContainer}>
       <TextInput
+      fontSize={16}
         placeholder="Email"
+        placeholderTextColor="#000000" 
         value={email}
         onChangeText={text => setEmail(text)}
         style={styles.input}
       />
       <TextInput
+      fontSize={16}
         placeholder="Password"
+        placeholderTextColor="#000000"
         value={password}
         onChangeText={text => setPassword(text)}
         style={styles.input}
@@ -82,24 +97,16 @@ const SignUpScreen = () => {
         onPress={handleLogin}
         style={styles.button}
       >
-        <Text style={styles.buttonText}>Login</Text>
+        <Text style={styles.buttonText}>SIGN IN</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        onPress={handleSignUp}
-        style={[styles.button, styles.buttonOutline]}
-      >
-        <Text style={styles.buttonOutlineText}>Register</Text>
-      </TouchableOpacity>
-
-    
-      <TouchableOpacity
-        onPress={handleSignOut}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Sign out</Text>
-      </TouchableOpacity>
-
     </View>
+
+
+    <TouchableOpacity
+        onPress={handleSignUp}>
+        <Text style={{color:'#000000',fontSize:16,paddingTop:10}}>Do not have any account yet?</Text>
+        <Text style={{color:'#FF367E',fontSize:16}}>Sign Up</Text>
+      </TouchableOpacity>
   </View>
 )
 }
@@ -111,44 +118,54 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: "#FFFFFF",
+  },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 1000,
   },
   inputContainer: {
-    width: '80%'
+    width: '80%',
+    paddingTop: 90,
   },
+
   input: {
-    backgroundColor: 'white',
+    backgroundColor: '#FFFFFF',
     paddingHorizontal: 15,
     paddingVertical: 10,
     borderRadius: 10,
-    marginTop: 5,
+    marginTop: 15,
+    marginBottom: 10,
   },
+
   buttonContainer: {
     width: '60%',
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 40,
+    marginTop: 50,
   },
   button: {
-    backgroundColor: '#0782F9',
+    backgroundColor: '#FF367E',
     width: '100%',
     padding: 15,
     borderRadius: 10,
     alignItems: 'center',
+    paddingTop:20,
   },
   buttonOutline: {
     backgroundColor: 'white',
     marginTop: 5,
     borderColor: '#0782F9',
-    borderWidth: 2,
+    borderWidth: 2, 
+
   },
   buttonText: {
-    color: 'white',
+    color: '#FFFFFF',
     fontWeight: '700',
     fontSize: 16,
   },
-  buttonOutlineText: {
-    color: '#0782F9',
-    fontWeight: '700',
-    fontSize: 16,
-  },
+
 })
