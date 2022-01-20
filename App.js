bb000aa9cde92427899d529ed6dfd374082cf05f
a_sweetscent <athirah.borhan2980@gmail.com>
2022-01-20 22:48:29 +0800

    Update App.js

--- App.js
+++ App.js
@@ -6,10 +6,6 @@ import HomeScreen from "./src/screens/HomeScreen";
 import ProductsScreen from "./src/screens/ProductsScreen";
 import SignUpScreen from './src/screens/SignUpScreen';
 import TransactionsScreen from './src/screens/TransactionsScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
-
-import { db } from './src/config/db';
-import firebase from 'firebase';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 import {auth} from './src/config/db'
 
@@ -17,7 +13,6 @@ import {auth} from './src/config/db'
 //import FormScreen from "./src/screens/FormScreen";
 
 const Drawer = createDrawerNavigator();
-
 
 export default class App extends Component {
 
@@ -41,7 +36,6 @@ export default class App extends Component {
         <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Products" component={ProductsScreen} />
         <Drawer.Screen name="Transactions" component={TransactionsScreen} />
-        <Drawer.Screen name="Profile" component={ProfileScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
     );
