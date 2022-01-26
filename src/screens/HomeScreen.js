e31dc464cb1887dbe613081d5ca6f9b1fc07c6c3
a_sweetscent <athirah.borhan2980@gmail.com>
2022-01-27 07:29:25 +0800

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,18 +7,32 @@ import {
   FlatList, 
   TouchableOpacity,
   SafeAreaView, 
-  RefreshControl } from "react-native";
+  RefreshControl,
+  Dimensions,
+  Alert,
+  Linking } from "react-native";
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faHome,faShoppingBag,faUser, faFileInvoiceDollar, faPlusCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { faHome,faShoppingBag, faSignOutAlt, faFileInvoiceDollar, faPlusCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import { useNavigation } from '@react-navigation/native';
-import {addProduct, getProduct, DeleteProduct} from '../api/productApi';
+import {getQuantity, getProduct} from '../api/productApi';
 import {auth} from '../config/db'
 import {ListItem, Divider} from 'react-native-elements'
 import ProductsScreen from "./ProductsScreen";
-import SignUpScreen from "./SignUpScreen";
+import SignInScreen from "./SignInScreen";
 import RNRestart from "react-native-restart";
+import moment from "moment";
+import firestore from '@react-native-firebase/firestore';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
 
 class HomeScreen extends Component {
+
 
 state = {
   productList: [],
@@ -29,11 +43,80 @@ state = {
   quantity: null,
   price: null,
   productlink: null,
-  ModalOpen: false
+  ModalOpen: false,
+  totals: [0,0,0,0,0,0,0]
 }
 
 _onRefresh() {
-  RNRestart.Restart()
+  getProduct(this.onItemReceived);
+  getQuantity(this.onGraphReceived,
+
+    moment(new Date()).subtract(6,'days').format("DD/MM/YY"),
+    moment(new Date()).subtract(5,'days').format("DD/MM/YY"),
+    moment(new Date()).subtract(4,'days').format("DD/MM/YY"),
+    moment(new Date()).subtract(3,'days').format("DD/MM/YY"),
+    moment(new Date()).subtract(2,'days').format("DD/MM/YY"),
+    moment(new Date()).subtract(1,'days').format("DD/MM/YY"),
+    moment(new Date()).format("DD/MM/YY")
+           )
+}
+
+
+
+onGraphReceived = (graph1, graph2, graph3, graph4, graph5, graph6, graph7) => { //updates graph
+
+  var total1=0, total2=0, total3=0, total4=0, total5=0, total6=0, total7=0;
+  var totals=[];
+
+  this.setState(prevState => ({
+    graph1: prevState.graph1 = graph1
+  }));
+
+
+  for (let i=0; i <graph1.length ; i++ ){
+  total1 =  total1 + graph1[i].sales 
+  console.log(graph1[i].sales)
+  }
+  for (let i=0; i <graph2.length ; i++ ){
+  total2 =  total2 + graph2[i].sales 
+  console.log(graph2[i].sales)
+  }
+  for (let i=0; i <graph3.length ; i++ ){
+  total3 =  total3 + graph3[i].sales 
+  console.log(graph3[i].sales)
+  }
+  for (let i=0; i <graph4.length ; i++ ){
+  total4 =  total4 + graph4[i].sales 
+  console.log(graph4[i].sales)
+  }
+  for (let i=0; i <graph5.length ; i++ ){
+  total5 =  total5 + graph5[i].sales 
+  console.log(graph5[i].sales)
+  }
+  for (let i=0; i <graph6.length ; i++ ){
+  total6 =  total6 + graph6[i].sales 
+  console.log(graph6[i].sales)
+  }
+  for (let i=0; i <graph7.length ; i++ ){
+  total7 =  total7 + graph7[i].sales 
+  console.log(graph7[i].sales)
+  }
+
+totals[0] = total1;
+totals[1] = total2;
+totals[2] = total3;
+totals[3] = total4;
+totals[4] = total5;
+totals[5] = total6;
+totals[6] = total7;
+
+this.setState(prevState => ({
+  totals: prevState.totals = totals
+}));
+
+  console.log("sales for 5 days")
+  console.log(totals)
+
 }
 
 onItemReceived = (productList) => {
@@ -47,17 +130,74 @@ onItemReceived = (productList) => {
 }
 
 componentDidMount() {
+ getQuantity(this.onGraphReceived,
 
+      moment(new Date()).subtract(6,'days').format("DD/MM/YY"),
+      moment(new Date()).subtract(5,'days').format("DD/MM/YY"),
+      moment(new Date()).subtract(4,'days').format("DD/MM/YY"),
+      moment(new Date()).subtract(3,'days').format("DD/MM/YY"),
+      moment(new Date()).subtract(2,'days').format("DD/MM/YY"),
+      moment(new Date()).subtract(1,'days').format("DD/MM/YY"),
+      moment(new Date()).format("DD/MM/YY")
+             )
  getProduct(this.onItemReceived); //calls the items from firestore
    }
 
 render() {
-
+const {totals} = this.state;
 
   return(
 <SafeAreaView style={styles.container}>
 <View style={styles.rect}>
-  <View style={styles.rect2}></View>
+  <View style={styles.rect2}>
+  <LineChart
+    data={{
+      
+      labels: [
+      moment(new Date()).subtract(6,'days').format("DD/MM"),
+      moment(new Date()).subtract(5,'days').format("DD/MM"),
+      moment(new Date()).subtract(4,'days').format("DD/MM"),
+      moment(new Date()).subtract(3,'days').format("DD/MM"),
+      moment(new Date()).subtract(2,'days').format("DD/MM"),
+      moment(new Date()).subtract(1,'days').format("DD/MM"),
+      moment(new Date()).format("DD/MM")
+       ],
+      datasets: [
+        {
+          data: totals
+     
+        }
+      ],
+      legend: ["Daily Sales"]
+    }}
+    
+    width={Dimensions.get("window").width} // from react-native
+    height={210}
+    yAxisInterval={1} // optional, defaults to 1
+    chartConfig={{
+     
+      backgroundGradientFrom: "#311D52",
+      backgroundGradientTo: "#602a70",
+      decimalPlaces: 0, 
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+     
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+       
+      },
+      propsForLabels: {
+        
+      }
+    }}
+    
+    style={{
+    
+    }}
+  />
+
+  </View>
   <View style={styles.rect3}>
     <View style={styles.iconRow}>
 
@@ -77,18 +217,23 @@ render() {
     </TouchableOpacity>
     
     <TouchableOpacity
-        onPress={() =>  this.props.navigation.navigate('Transactions')}
+        onPress={() =>  this.props.navigation.navigate('Records')}
         style={styles.circle}>
     <FontAwesomeIcon icon={faFileInvoiceDollar} size={32} style={styles.icon1}></FontAwesomeIcon>
     <Text style={styles.icontext}>Records</Text>
     </TouchableOpacity>
-    
+
     <TouchableOpacity
-        onPress={() =>  this.props.navigation.navigate('Profile')}
+        onPress={() => {auth.signOut().then(() => 
+        this.props.navigation.navigate("SignIn"))
+        console.log('Logged out');
+        RNRestart.Restart();}}
         style={styles.circle}>
-    <FontAwesomeIcon icon={faUser} size={32} style={styles.icon1}></FontAwesomeIcon>
-    <Text style={styles.icontext}>Profile</Text>
+
+    <FontAwesomeIcon icon={faSignOutAlt} size={32} style={styles.icon1}></FontAwesomeIcon>
+    <Text style={styles.icontext}>Log Out</Text>
     </TouchableOpacity>
+   
     </View>
   </View>
 <View style={{flex:1}}>
@@ -108,7 +253,28 @@ render() {
        
         return(
 
-          <View style={styles.rect4}>
+          <TouchableOpacity 
+          style={styles.rect4}
+          onLongPress={() => 
+            {
+              Alert.alert(
+              "Place an order?", //alert title
+               'This will bring you to an external webpage', //alert desc
+              [ //buttons
+              {
+               text: "Ok",
+               onPress: () => {{
+                Linking.openURL(item.productlink);
+               }}
+              },
+              {
+                text: "Cancel",
+                onPress: () => console.log("Cancel Pressed"),
+                style: "cancel"
+              },
+              ],
+                 {cancelable: true} //lets alert be closed from outward taps
+              );}}>
 
           <Image source={item.picture}  style={styles.avatar} />
           
@@ -117,7 +283,7 @@ render() {
             <Text style={{fontWeight:"bold",  fontSize: 40, color: '#969696'}}>{item.quantity} IN STOCK</Text>
         
             </View>
-        </View>
+        </TouchableOpacity>
         )
         
       }
@@ -126,12 +292,6 @@ render() {
 </View>
 
 </View>
-
-
-
-
-
-   
    
 
 </SafeAreaView>
@@ -162,7 +322,8 @@ const styles = StyleSheet.create({
   },
   rect2: {
    height: 240,
-    backgroundColor: "#311D52"
+    backgroundColor: "black",
+    backgroundColor: "#311D52",
   },
   rect3: {
     height: 120,
