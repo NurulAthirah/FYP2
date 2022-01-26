59234614fcbc47a1694389160cea4c43282d458f
a_sweetscent <athirah.borhan2980@gmail.com>
2022-01-27 07:28:05 +0800

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { MainStackNavigator, SubStackNavigator } from './navigation/StackNavigator';
 import BottomTabNavigator from './navigation/TabNavigator';
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import ProductsScreen from "./src/screens/ProductsScreen";
 import SignUpScreen from './src/screens/SignUpScreen';
@@ -17,30 +17,21 @@ import {auth} from './src/config/db';
 
 const Drawer = createDrawerNavigator();
 
+const Stack = createStackNavigator();
+
 export default class App extends Component {
 
   
   render() {
     return (
       <NavigationContainer>
-       <Drawer.Navigator initialRouteName="SignIn"drawerContent={props => { //Nanti tukar initial route name to SIgnup balik
-    return (
-      <DrawerContentScrollView {...props}>
-        <DrawerItemList {...props} />
-        <DrawerItem label="Logout" onPress={() => {
-
-      auth.signOut().then(() => props.navigation.navigate("SignUp"))
-      console.log('Logged out');
-      }} />
-      </DrawerContentScrollView>
-    )
-  }}>
-      <Drawer.Screen name="SignIn"  options={{drawerItemStyle: { height: 0 }, headerShown: false }} component={SignInScreen} />
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Products" component={ProductsScreen} />
-        <Drawer.Screen name="Records" component={TransactionsScreen} />
-        <Drawer.Screen name="Sign Up" options={{ drawerItemStyle: { height: 0 }, headerShown: false }} component={SignUpScreen} />
-      </Drawer.Navigator>
+      <Stack.Navigator>
+      <Stack.Screen name="SignIn" options={{headerShown: false}} component={SignInScreen} />
+      <Stack.Screen name="SignUp" options={{headerShown: false}} component={SignUpScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Products" component={ProductsScreen} />
+      <Stack.Screen name="Records" component={TransactionsScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
     );
   }
