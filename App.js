c464a302d27ad8d4dc9ec1029dfb5bf6c4c12a0e
a_sweetscent <athirah.borhan2980@gmail.com>
2022-01-26 16:11:21 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import 'react-native-gesture-handler';
 import React, { Component } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { MainStackNavigator, SubStackNavigator } from './navigation/StackNavigator';
+import BottomTabNavigator from './navigation/TabNavigator';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import ProductsScreen from "./src/screens/ProductsScreen";
 import SignUpScreen from './src/screens/SignUpScreen';
+import SignInScreen from './src/screens/SignInScreen';
 import TransactionsScreen from './src/screens/TransactionsScreen';
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
-import {auth} from './src/config/db'
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem, drawerItemStyle } from '@react-navigation/drawer';
+import {auth} from './src/config/db';
 
 
 //import FormScreen from "./src/screens/FormScreen";
@@ -20,7 +23,7 @@ export default class App extends Component {
   render() {
     return (
       <NavigationContainer>
-      <Drawer.Navigator initialRouteName="SignUp"drawerContent={props => { //Nanti tukar initial route name to SIgnup balik
+       <Drawer.Navigator initialRouteName="SignIn"drawerContent={props => { //Nanti tukar initial route name to SIgnup balik
     return (
       <DrawerContentScrollView {...props}>
         <DrawerItemList {...props} />
@@ -32,12 +35,15 @@ export default class App extends Component {
       </DrawerContentScrollView>
     )
   }}>
-      <Drawer.Screen name="SignUp"  options={{ headerShown: false }} component={SignUpScreen} />
+      <Drawer.Screen name="SignIn"  options={{drawerItemStyle: { height: 0 }, headerShown: false }} component={SignInScreen} />
         <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Products" component={ProductsScreen} />
-        <Drawer.Screen name="Transactions" component={TransactionsScreen} />
+        <Drawer.Screen name="Records" component={TransactionsScreen} />
+        <Drawer.Screen name="Sign Up" options={{ drawerItemStyle: { height: 0 }, headerShown: false }} component={SignUpScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
     );
   }
 }
+
+/****/
